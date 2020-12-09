b905873cfc640f30179fec2bed33fd16c1259b4a
Owen <owen@lunatechnw.com>
2020-12-09 21:07:40 +0000

    commiting changes for making the pluging actaully work

--- owen-plugin.php
+++ owen-plugin.php
@@ -3,10 +3,18 @@
 * Plugin Name: owen-plugin
 * Description: Piano
 */
-define( 'OP_PIANO_PLUGIN_PATH', untrailingslashit( plugin_dir_url( __FILE__ ) ));
-add_action( 'admin_menu', 'customer_menu_pages');
-// add_action('admin_enqueue_scripts', 'piano');
 
+define( 'OP_PIANO_PLUGIN_PATH', untrailingslashit( plugin_dir_url( __FILE__ ) ));
+
+define( 'OP_PIANO_PLUGIN_FILE', __FILE__ );
+define( 'OP_PIANO_PLUGIN_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ));
+define( 'OP_PIANO_PLUGIN_URL', plugins_url( 'OP_PIANO_PLUGIN_NAME', __FILE__ ) );
+define( 'OP_PIANO_PLUGIN_NAME', 'owen-plugin');
+// define( 'KF_PLUGIN_URL', plugins_url( KF_PLUGIN_NAME ) );
+// For template redirects
+
+$plugin_path = untrailingslashit( plugin_dir_path( __FILE__ ) );
+add_action( 'admin_menu', 'customer_menu_pages');
 
 function customer_menu_pages(){
   add_menu_page( 'Piano', 'Piano', 'manage_options', 'piano', 'customer_admin_page', 'dashicons-format-audio', 0);
@@ -14,23 +22,38 @@ function customer_menu_pages(){
 
 add_shortcode('piano_shortcode', "piano_shortcode"); 
 
-function piano_shortcode(){
+function piano_shortcode() {
+ 
+
+}
+
+function customer_admin_page() {
   wp_enqueue_script('piano_js', OP_PIANO_PLUGIN_PATH . "/piano_project/main_script.js", ['jquery']);
   wp_localize_script('piano_js', 'something', OP_PIANO_PLUGIN_PATH);
   wp_enqueue_style('piano_css', OP_PIANO_PLUGIN_PATH . "/piano_project/main_style.css");
-  require 'index.php';
-  // $content = "<h1>DIGITAL PIANO</h1>
-  // <div id='piano_keys'>
-  // </div>";
-  // return $content;
-}
 
-function customer_admin_page(){
+  $piano = '
+    <header>DIGITAL PIANO</header>
+
+    <div id="piano_keys"></div></br>
+
+    <div>
+      </br>
+      <button id="playsong">Play Song</button>
+      <button id="addrow">Add Row</button></br></br>
+      <button id="faster">Faster</button>
+      <button id="slower">Slower</button>
+      <var id="speed">Speed:1</var>
+    </div></br>
+
+    <div id="piano_roll"></div>
+
+    <div id="filltest"></div>
+  ';
+
+echo $piano;
+
+echo plugins_url('kjhgfd', __FILE__);
 };
-
-
-
-
-
 
 ?>

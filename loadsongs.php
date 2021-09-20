3268ddcc7b20eaa78557f0fa58442308862c7b62
Owen Palmer <owen@lunatechnw.com>
2021-09-20 19:22:23 +0000

    Set limit to 21 for songs query

--- loadsongs.php
+++ loadsongs.php
@@ -2,13 +2,14 @@
 include('/var/www/owenpalmer.com/htdocs/wp-load.php');
 global $wpdb;
 $test = $_POST['test'];
+$test = 21;
 
 $load = $wpdb->get_results(
     $wpdb->prepare(
         "SELECT * FROM wp_piano LIMIT %d",
         $test)
 );
-
+r($load);
 wp_send_json($load);
 
 ?>
